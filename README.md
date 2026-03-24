c94f01084405654b52e4b4fed3faacb523730553
wanggy <819656126@qq.com>
2026-03-24 10:10:14 +0800

    第1-6章的代码

---
+++ README.md
@@ -0,0 +1,4 @@
+# ros2bookcode
+ROS 2机器人开发：从入门到实践        学习过程中的相关代码
+
+![book](https://github.com/user-attachments/assets/a667b934-23e2-4106-9cca-076e12b44359)
---
+++ chapt2/ros_python_node.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import Node
+
+
+def main():
+    rclpy.init()
+    node = Node('python_node')
+    node.get_logger().info('Hello, ROS 2 from Python!')
+    rclpy.spin(node)
+    # node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
